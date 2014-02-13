1ee35031c279d0af4cd83d81049b00807ee7a802
Jakub Racek <me@jakubracek.net>
2014-02-13 23:18:00 +0000

    remove before_save callback

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -4,9 +4,6 @@ class Delivery < ActiveRecord::Base
 
   validates_numericality_of :lat, :lng, allow_blank: true
 
-  #Run before-save callback to set lat and lng
-  before_save :set_lat_lng_remotely
-
   # Actually this method should return address as one-line string built from
   # address parameters like House number, street, post-code but in this example
   # I use just post_code so it's basically just proxy getter for post_code
