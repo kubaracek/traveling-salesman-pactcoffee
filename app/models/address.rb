22e503ee988656949b42cb48f4bc9ab5c0a33512
Jakub Racek <me@jakubracek.net>
2014-02-13 20:33:40 +0000

    Added comment for get_address method

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,9 @@ class Address < ActiveRecord::Base
 
   validates_numericality_of :lat, :lng, allow_blank: true
 
+  # Actually this method should return address as one-line string built from
+  # address parameters like House number, street, post-code but in this example
+  # I use just post_code so it's basically just proxy getter for post_code
   def get_address
     return "#{post_code}"
   end
